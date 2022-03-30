8516d4be8900d072850c8ba8c13336419d666bf6
Nick Weiner <nicholas.weiner@icloud.com>
2022-03-30 18:46:21 -0400

    updates to calculate.js and scripts.js , readme

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # Cryptocurrency Budget Tool
 
+## Installation
+
+Run `npm install`.
+
+## Running the App
+
+Run `npm start`
+
+Open the `public/index.html`
+
+## About
+
+I designed this website because I wanted a tool that would help me from save money when buying crypto each month. My excitement toward everything blockchain often leads to me investing too much each month. In partial completion of the Code Louisville Web Development 2 class, I have worked on this budget tool to save me from myself (and possibly help some friends).
+
+## Feature List:
+
+1. Read and parse an external file (such as JSON or CSV) into your application and display some data from that in your app
+
+Location: ` public/assets/js/main.js `
+
+2. Retrieve data from an external API and display data in your app (such as with fetch() or with AJAX)
+
+Location: ` public/assets/js/calculate.js `
+
+3. Create an array, dictionary or list, populate it with multiple values, retrieve at least one value, and use or display it in your application
+
+Location: 
+
+4. Create a form and save the values (on click of Submit button) to an external file 
+You must show us or document where that information is being stored so we can confirm that it’s being saved/persisted
+
+Location: ` public/assets/js/main.js ` & ` public/tool.html ` , ` data/app.json `
+
+DEV API Location: `public/assets/js/api.js` , `server/index.js` & `public/assets/js/main.js `
+
+5. Implement a regular expression (regex) to ensure a field either a phone number or an email address is always stored and displayed in the same format
+
+Location: ` public/assets/js/emailvalidate.js ` , `public/tool.html `
+
+## Additional Features:
+
+- Bootstrap 5 Responsive Layout
+
+
+## Credit
+Form made with server-side help from Justin Prince's Dev API (https://github.com/justinkprince/dev-api). 
+
+
--- public/assets/js/calculate.js
+++ public/assets/js/calculate.js
@@ -1,16 +1,54 @@
-const CoinGecko = require('coingecko-api');
+// Favorite Color One
+const favCrypto = document.getElementById("favCrypto");
+let favCryptoOptions = cryptoOptions;
 
-const CoinGeckoClient = new CoinGecko();
-    let data = CoinGeckoClient.exchanges.fetchTickers('bitfinex', {
-        coin_ids: ['bitcoin']
-    });
-    var _coinList = {};
-    var _datacc = data.data.tickers.filter(t => t.target == 'USD');
-    [
-        'BTC'
-    ].forEach((i) => {
-        var _temp = _datacc.filter(t => t.base == i);
-        var _res = _temp.length == 0 ? [] : _temp[0];
-        _coinList[i] = _res.last;
+for(let i = 0; i < favCryptoOptions.length; i++) {
+let opt = favCryptoOptions[i];
+const el = document.createElement("option");
+el.textContent = opt;
+el.value = opt;
+favCrypto.appendChild(el);
+}
+
+getCoins();
+
+function getCoins() {
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            let coins = data.map(el => el);
+
+            for (let i = 0; i < coins.length * .2; i++) {
+                coins.pop();
+            }
+
+            displayCoins(coins);
+
+
+        })
+}
+
+function displayCoins(coins) {
+    const list = document.querySelector('.section2');
+
+
+    coins.forEach(coin => {
+        let sym = document.createElement('p');
+        sym.classList.add('coin-name');
+        let price = document.createElement('p');
+        price.classList.add('coin-price');
+
+        sym.innerHTML = coin.symbol;
+        price.innerHTML = '$' + coin.current_price;
+
+        list.appendChild(sym);
+        list.appendChild(price);
+        
+
+        // Use this data to create an array with symbols and current_price
+        console.log(coin.symbol)
+        console.log(coin.current_price)
     })
-console.log(_coinList);
+
+}
