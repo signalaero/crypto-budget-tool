1a4b3e0f3136851afbca32d6a2ab27d8a1999e6e
Nick Weiner <nicholas.weiner@icloud.com>
2022-04-01 08:14:15 -0400

    deleted constructor

--- public/assets/js/calculate.js
+++ public/assets/js/calculate.js
@@ -1,8 +1,5 @@
 class Calculate
 {
-    constructor() {
-    }
-    
     getCoins() {
         const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'`
         fetch(url)
@@ -14,11 +11,12 @@ class Calculate
                     coins.pop();
                 }
                  return displayCoins(coins);
+ 
 
             })
+    
     }
 
-    
     calculation()
     {
         // Calculations
